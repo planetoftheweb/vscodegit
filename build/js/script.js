537ac1238db46b5d80a4e5be5bec9daf91e137fc
Ray Villalobos <ray@planetoftheweb.com>
2022-12-13 13:36:50 -0500

    Added a sample TODO for creating an issue.

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,7 @@ $(function () {
   let meetMonsters = document.querySelector('#meet')
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
   let navHeight = nav.scrollHeight
+  // TODO: #6 Update these names to make them clearer
 
   monsterScroll.forEach(
     (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
